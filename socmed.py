dbeac903de3bd5844d879c831742636a0fa8f42b
binsoy69 <71478161+binsoy69@users.noreply.github.com>
2021-04-09 12:36:07 +0800

    Update socmed.py
    Messenger link was changed

--- socmed.py
+++ socmed.py
@@ -4,7 +4,7 @@
 import webbrowser
 
 facebook = 'https://www.facebook.com/'
-messenger = 'https://www.messenger.com/t/3205784576217717/'
+messenger = 'https://www.messenger.com/'
 twitter = 'https://twitter.com/home'
 insta = 'https://www.instagram.com/'
 
